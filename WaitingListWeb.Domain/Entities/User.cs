7c9d357c1d82feae968a44fc203df8c1b09b5096
duynp <duynguyen04302004@gmail.com>
2025-11-12 21:01:39 +0700

    first commit

---
+++ WaitingListWeb.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaitingListWeb.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ WaitingListWeb.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaitingListWeb.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            SharedServiceContainer.AddSharedServices<WaitingListDbContext>(services, configuration, configuration["MySerilog:Filename"]);
+            return services;
+        }
+    }
+}
